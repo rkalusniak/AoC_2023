1cf0fe7c6bd4ed4600d0f99084b8660a31310e7c
rkalusniak <rachel.kalusniak@gmail.com>
2024-03-26 23:17:33 -0400

    finish Day 1B puzzle

--- Day1/Puzzle1b.py
+++ Day1/Puzzle1b.py
@@ -1,15 +1,15 @@
 """
-Created: March 26 08:17 2024
+Created: March 26 20:17 2024
 
 by: Rachel Kalusniak
 """
-import inflect
+from word2number import w2n
 import re
 
 # Allow to run with test or actual file
 test = False
 if test:
-    data_file = 'input_test.txt'
+    data_file = 'input_testb.txt'
 else:
     data_file = 'input.txt'
 
@@ -17,19 +17,18 @@ else:
 with open(data_file, 'r') as f:
     input = f.read().splitlines()
 
-# Create an engine to read words to numbers
-p = inflect.engine()
+# Detect numbers in the string
+# Create pattern and add ?= for overlap
+num_pattern = '(?=(one|two|three|four|five|six|seven|eight|nine|\d))'
+compile_pattern = re.compile(num_pattern)
 
-numbers = [[int(x) for x in re.findall("\d", i)] for i in input]
+# Find all numbers and number words and convert to integer
+numbers = [[w2n.word_to_num(x) for x in re.findall(num_pattern, i)] for i in input]
 
 # Find two-digit numbers with first and last value
 two_digit = []
 for j in numbers:
     two_digit.append(j[0]*10 + j[-1])
 
-# Find the sum of two digit list
+# # Find the sum of two digit list
 print(sum(two_digit))
-
-word = "three"
-number = p.number_to_words(word)
-print(number)
